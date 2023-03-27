639a50565f57624c70d064a05fd04dfb8e065ef5
asharanjith <ashasvijayan@gmail.com>
2023-03-27 22:30:22 +0400

    Add recipes table and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :recipes
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'users#index'
+  root 'recipes#index'
 
   resources :users
 
